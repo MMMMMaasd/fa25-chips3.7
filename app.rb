361dfd9b9da4ecf954b58838e53d15d07b46d7ac
Michael Bian <zb2253@nyu.edu>
2025-09-26 20:05:51 +0000

    update

--- app.rb
+++ app.rb
@@ -39,8 +39,14 @@ class WordGuesserApp < Sinatra::Base
   # If a guess is repeated, set flash[:message] to "You have already used that letter."
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
-    params[:guess].to_s[0]
-    ### YOUR CODE HERE ###
+    guess_letter = params[:guess].to_s[0]
+    begin
+      unless @game.guess(guess_letter)
+        flash[:message] = "You have already used that letter."
+      end
+    rescue ArgumentError
+      flash[:message] = "Invalid guess."
+    end
     redirect '/show'
   end
 
@@ -51,7 +57,14 @@ class WordGuesserApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    case @game.check_win_or_lose
+    when :win
+      redirect '/win'
+    when :lose
+      redirect '/lose'
+    when :play
+      erb :show
+    end
   end
 
   get '/win' do
